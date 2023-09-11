f3e0bf899a895dccd6729fd911408b9f877f58d0
saurav_gupta <saurav.gupta@redhuntlabs.com>
2023-09-11 14:28:42 +0530

    Added grafana

--- kafka_exporter.py
+++ kafka_exporter.py
@@ -4,14 +4,15 @@ from confluent_kafka.admin import AdminClient
 
 conf = {"bootstrap.servers": "172.31.27.25:9093"}
 ac = AdminClient(conf)
-sleep_time = 30
+sleep_time = 5
+port = 9200
+
 
 if __name__ == "__main__":
     # Start up the server to expose the metrics.
-    port = 9200
     start_http_server(port)
     scan_tools_enum = Enum(
-        "scan-tools-consumers-state",
+        "scan_tools_consumers_state",
         "Scan tools Consumer Group State",
         states=["stable", "rebalance"],
     )
@@ -23,6 +24,7 @@ if __name__ == "__main__":
         groups = ac.describe_consumer_groups(group_ids=["trigger_group_prod"])
         group_desc = groups["trigger_group_prod"].result()
         curr_state = group_desc.state
+
         if "PREPARING_REBALANCING" in str(curr_state):
             scan_tools_enum.state("rebalance")
         else:
